ca8cc864c4bf9656fe49ac2fdaa8a0f1d57fe43d
Carol <carolhersant4@gmail.com>
2021-06-30 14:31:01 +0100

    Estilo del tv

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+import { TvComponent } from './tv/tv.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'nav',
+    component: NavComponent,
+  },
+  {
+    path: 'tv',
+    component: TvComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
